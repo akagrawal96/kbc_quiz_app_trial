c6a69fd0ce4f6ca29e32bcd1282657017dc94351
akagrawal96 <agrawalsirj@gmail.com>
2023-08-11 07:04:31 +0530

    day2 - data not change if already exists.

--- lib/services/firedb.dart
+++ lib/services/firedb.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 
 class FireDB{
 
@@ -8,17 +9,39 @@ final FirebaseAuth _auth = FirebaseAuth.instance;
   createNewUser(String name, String email, String mobile, String photoUrl,String uid) async {
     final User? currentUser = _auth.currentUser;
 
-    await FirebaseFirestore.instance.collection('user').doc(currentUser!.uid).set(
-      {
-      "name":name,
-      "email":email,
-      "photo":photoUrl,
-      "mobile":mobile,
-      "money":"5000",
-      "uid": currentUser.uid,
+    if(await getUser()){
+      debugPrint("user already exists");
+    }else{
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(currentUser!.uid)
+          .set({
+        "name": name,
+        "email": email,
+        "photo": photoUrl,
+        "mobile": mobile,
+        "money": "50000",
+        "uid": currentUser.uid,
+      }).then((value) => "User Registered Successfully");
     }
-    ).then((value) => "User Registered Successfully")
-    ;
+
+    
+  }
+
+  Future<bool> getUser() async{
+    final User? currentUser = _auth.currentUser;
+
+    String user = "";
+    await FirebaseFirestore.instance.collection('users').doc(currentUser!.uid).get().then((value) {
+      user = value.data().toString();
+    });
+
+    if(user.toString() == "null"){
+      return false;
+    }else{
+      return true;
+    }
+
   }
 
   
